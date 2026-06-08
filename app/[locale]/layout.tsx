ea24f61dd4b7b314b77279e464b4e0612c5ced36
Pan-zhong <p-a-n-zhong@outlook.com>
2026-06-08 21:10:30 +1000

    fix deployment problem

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,9 +1,13 @@
 import {NextIntlClientProvider, hasLocale} from "next-intl";
 import {notFound} from "next/navigation";
-import {getMessages} from "next-intl/server";
+import {getMessages, setRequestLocale} from "next-intl/server";
 import {Navigation} from "@/components/layout/Navigation";
 import {Footer} from "@/components/layout/Footer";
 import {locales} from "../../i18n";
+
+export function generateStaticParams() {
+  return locales.map((locale) => ({locale}));
+}
 
 export default async function LocaleLayout({
   children,
@@ -17,6 +21,7 @@ export default async function LocaleLayout({
     notFound();
   }
 
+  setRequestLocale(locale);
   const messages = await getMessages();
 
   return (
