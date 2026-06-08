b27bb84cf6920079cefe6572394fe5d0001956cb
Pan-zhong <p-a-n-zhong@outlook.com>
2026-06-08 22:13:47 +1000

    fix deployment bug

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -25,7 +25,7 @@ export default async function LocaleLayout({
   const messages = await getMessages();
 
   return (
-    <NextIntlClientProvider messages={messages}>
+    <NextIntlClientProvider locale={locale} messages={messages}>
       <Navigation />
       {children}
       <Footer />
